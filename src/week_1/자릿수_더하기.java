35f8b8c6746b9c7b376edac22aba8e0d989e657d
Ga0Lee <leegy0419@gmail.com>
2024-11-06 22:39:48 +0900

    [해설] 나머지 연산자(%10)를 통해 일의 자리 숫자를 얻어내고, 10으로 나눠주는 과정을 n이 0보다 클 때까지 반복한다.

---
+++ src/week_1/자릿수_더하기.java
@@ -0,0 +1,16 @@
+package week_1;
+
+public class 자릿수_더하기 {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+
+            while (n > 0) {
+                answer += n % 10;
+                n /= 10;
+            }
+
+            return answer;
+        }
+    }
+}
