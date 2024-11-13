739f8d9c5cac0a90fcbb8d1d86a9aaf42aed6f2f
Ga0Lee <leegy0419@gmail.com>
2024-11-13 23:59:28 +0900

    [최소 직사각형 풀이] [w, h] 값 중 큰 값을 w로 치환한다.

---
+++ src/week_1/최소직사각형.java
@@ -0,0 +1,25 @@
+package week_1;
+
+import java.util.Arrays;
+
+public class 최소직사각형 {
+    class Solution {
+        public int solution(int[][] sizes) {
+            int answer = 0;
+
+            int[] rows = new int[sizes.length];
+            int[] cols = new int[sizes.length];
+
+            for (int i = 0; i < sizes.length; i++) {
+                rows[i] = Math.max(sizes[i][0], sizes[i][1]);
+                cols[i] = Math.min(sizes[i][0], sizes[i][1]);
+            }
+
+            Arrays.sort(rows);
+            Arrays.sort(cols);
+
+            answer = rows[rows.length - 1] * cols[cols.length - 1];
+            return answer;
+        }
+    }
+}
