f09df16d758d9ce779c66118677c1e56ea353bfc
Ga0Lee <leegy0419@gmail.com>
2024-11-14 00:01:07 +0900

    삼진법

---
+++ src/week_1/삼진법.java
@@ -0,0 +1,18 @@
+package week_1;
+
+public class 삼진법 {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+
+            String temp = "";
+            while(n!=0){
+                int num = n%3;
+                temp += Integer.toString(num);
+                n /= 3;
+            }
+            answer = Integer.parseInt(temp, 3);
+            return answer;
+        }
+    }
+}
