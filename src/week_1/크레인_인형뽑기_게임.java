ffb7f3335f993ebc7125a0d9062095f7ca92c465
Ga0Lee <leegy0419@gmail.com>
2024-11-14 22:25:51 +0900

    delete main

--- src/week_1/크레인_인형뽑기_게임.java
+++ src/week_1/크레인_인형뽑기_게임.java
@@ -30,20 +30,4 @@ public class 크레인_인형뽑기_게임 {
             return answer;
         }
     }
-
-    public static void main(String[] args) {
-        int[][] board = {
-                {0, 0, 0, 0, 0},
-                {0, 0, 1, 0, 3},
-                {0, 2, 5, 0, 1},
-                {4, 2, 4, 4, 2},
-                {3, 5, 1, 3, 1}
-        };
-
-        int[] move = {1, 5, 3, 5, 1, 2, 1, 4};
-
-        Solution solution = new Solution();
-
-        solution.solution(board, move);
-    }
 }
