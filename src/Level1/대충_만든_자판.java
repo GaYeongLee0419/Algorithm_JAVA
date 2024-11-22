ffe62f56a70f88517789ace678f9e226f699e96d
Ga0Lee <leegy0419@gmail.com>
2024-11-23 02:30:48 +0900

    [대충 만든 자판 풀이] 각 문자별로 인덱스를 구해준다.
    인덱스 구하는 과정
    
    1. keymap의 문자열을 합친 다음 중복을 제거하여 키를 추출한다. **ch**
    2. **ch** 별로 최소 인덱스를 찾고 map에 저장한다.

---
+++ src/Level1/대충_만든_자판.java
@@ -0,0 +1,53 @@
+package Level1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class 대충_만든_자판 {
+    public int[] solution(String[] keymap, String[] targets) {
+        // 키 배열에서 중복된 문자 제거
+        Map<Character, Integer> map = new HashMap<>();
+        String keyString = String.join("", keymap);
+
+        // 문자 : 최소 인덱스 매핑
+        keyString.chars()
+                .distinct()
+                .forEach(ch -> {
+                    int idx = Integer.MAX_VALUE;
+                    for (String keys : keymap) {
+                        int temp = keys.indexOf(ch);
+                        if (temp != -1) {
+                            idx = Math.min(idx, temp);
+                        }
+                    }
+                    map.put((char) ch, idx + 1); // 키에 대해 최소 인덱스 + 1 저장
+                });
+
+        // targets 배열에 대해 각 문자 합산 계산
+        return Stream.of(targets)
+                .mapToInt(target -> {
+                    int total = 0;
+                    for (char ch : target.toCharArray()) {
+                        if (map.containsKey(ch)) {
+                            total += map.get(ch);
+                        } else {
+                            return -1; // 중복이 없으면 -1 반환
+                        }
+                    }
+                    return total;
+                })
+                .toArray();
+    }
+}
+
+    public static void main(String[] args) {
+        String[] keyMap = {"ABACD", "BCEFD"};
+        String[] targets = {"ABCD", "AABB"};
+
+        대충_만든_자판 sol = new 대충_만든_자판();
+        int[] result = sol.solution(keyMap, targets);
+        Arrays.stream(result).forEach(System.out::println);
+    }
+
