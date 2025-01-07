498c5d7d9df09d3d41d56e5642db855126b1eb77
Ga0Lee <leegy0419@gmail.com>
2025-01-07 23:26:44 +0900

    [일곱 난쟁이] 전체 합에서 두 숫자를 골라 뺐을 때 100이 되는 두 수를 찾는다. → 특정 값을 찾는다 → 투포인터

---
+++ src/정렬/일곱_난쟁이.java
@@ -0,0 +1,43 @@
+package 정렬;
+
+import java.io.*;
+import java.util.*;
+
+import static java.lang.System.out;
+
+public class 일곱_난쟁이 {
+    public static void solution(int[] data){
+        int total = Arrays.stream(data).sum();
+
+        Arrays.sort(data);
+        int start = 0;
+        int end = data.length - 1;
+
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+
+            int temp = total - (data[start] + data[end]);
+            if (temp == 100) {
+                for (int height : data) {
+                    if (height != data[start] && height != data[end])
+                        sb.append(height).append("\n");
+                }
+                System.out.println(sb);
+                return;
+            }
+            if (temp > 100) start++;
+            else end--;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] heights = new int[9];
+
+        for (int i = 0; i < 9; i++)
+            heights[i] = Integer.parseInt(br.readLine());
+
+        solution(heights);
+    }
+}
