c5893f87f648c8912a4d570bc9d5d3a3eaf87ba9
Ga0Lee <leegy0419@gmail.com>
2025-02-24 22:47:26 +0900

    투포인터 풀이 + 이분탐색 풀이

--- src/TwoPointer/BaekJoon7795.java
+++ src/TwoPointer/BaekJoon7795.java
@@ -17,20 +17,55 @@ public class BaekJoon7795 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < t; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+
             a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
             b = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
 
-            System.out.println();
+//            int count = 0;
+//            for (int target : a) {
+////                System.out.println(target + " : " + binarySearch(target));
+//                count += binarySearch(target);
+//            }
+//
+//            System.out.println(count);
+            System.out.println(twoPointer());
         }
     }
 
-    static int twoPointer() {
-        int left = 0, right = 0;
+    static int binarySearch(int target) {
+        int start = 0, end = m - 1, count = 0;
 
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (b[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return start;
+    }
+
+    static int twoPointer() {
+        int pointA = n - 1, pointB = m - 1, count = 0;
+
+        while (pointA >= 0 && pointB >= 0) {
+            if (a[pointA] > b[pointB]) {
+                count += pointB + 1;
+                pointA--;
+            } else {
+                pointB--;
+            }
+        }
+        return count;
     }
 }
