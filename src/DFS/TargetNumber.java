7823ebba872c50a4d454aa02d1f506772745bbec
Ga0Lee <leegy0419@gmail.com>
2025-03-06 23:51:48 +0900

    feat: Programmers 타겟 넘버 bfs 풀이

--- src/DFS/TargetNumber.java
+++ src/DFS/TargetNumber.java
@@ -1,5 +1,8 @@
 package DFS;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TargetNumber {
     public int solution(int[] numbers, int target) {
         return dfs(0, numbers, target, 0);
@@ -13,5 +16,29 @@ public class TargetNumber {
 
         return dfs(depth + 1, numbers, target, sum + numbers[depth])
                 + dfs(depth + 1, numbers, target, sum - numbers[depth]);
+    }
+
+    private int bfs(int[] numbers, int target) {
+        int result = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0);
+
+        for (int i = 0; i < numbers.length; i++) {
+            int size = queue.size();
+
+            for (int j = 0; j < queue.size(); j++) {
+                int sum = queue.poll();
+                queue.add(sum + numbers[i]);
+                queue.add(sum - numbers[i]);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            if (queue.poll() == target)
+                result++;
+        }
+
+        return result;
     }
 }
