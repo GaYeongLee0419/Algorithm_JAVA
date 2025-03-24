a8d75a7b31b6ac84bca471ee9961a09ceb18d429
Ga0Lee <leegy0419@gmail.com>
2025-03-24 23:46:14 +0900

    feat: Programmers 이중우선순위큐 문제 풀이 - 정확성 100.0

--- src/Queue/DoubleQueue.java
+++ src/Queue/DoubleQueue.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class DoubleQueue {
     PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
     PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-    Map<Integer, Integer> countMap = new HashMap<>();
 
     public int[] solution(String[] operations) {
         for (String operation : operations) {
@@ -30,46 +29,39 @@ public class DoubleQueue {
 
         }
 
-        if (maxHeap.isEmpty() && minHeap.isEmpty()) {
+        if (minHeap.isEmpty()) {
             return new int[] {0, 0};
+        } else if (minHeap.size() == 1) {
+            int num = minHeap.poll();
+            return new int[] {num, num};
+        } else {
+            int max = popMaxValue();
+            int min = popMinValue();
+
+            return new int[] {max, min};
         }
 
-        int maxValue = popMaxValue();
-        int minValue = popMinValue();
-
-        return new int[] {maxValue, minValue};
     }
 
     public void push(int num) {
         maxHeap.offer(num);
         minHeap.offer(num);
-        countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-    }
-
-    public int poll(Queue<Integer> queue) {
-        int result = 0;
-
-        while (!queue.isEmpty()) {
-            int number = queue.poll();
-            int count = countMap.getOrDefault(number, 0);
-
-            if (count == 0) continue;
-            if (count == 1) countMap.remove(number);
-            else countMap.put(number, count - 1);
-
-            result = number;
-            break;
-        }
-
-        return result;
     }
 
     public int popMaxValue() {
-        return poll(maxHeap);
+        if (maxHeap.isEmpty()) return Integer.MIN_VALUE;
+        int num = maxHeap.poll();
+        minHeap.remove(num);
+
+        return num;
     }
 
     public int popMinValue() {
-        return poll(minHeap);
+        if (minHeap.isEmpty()) return Integer.MAX_VALUE;
+        int num = minHeap.poll();
+        maxHeap.remove(num);
+
+        return num;
     }
 
     public static void main(String[] args) {
