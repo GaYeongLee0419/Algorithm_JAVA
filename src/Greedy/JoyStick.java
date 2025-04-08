23c54d29a3fa5419a877b5ff8d95a7934b94cd19
Ga0lee <leegy0419@gmail.com>
2025-04-08 22:48:57 +0900

    refactor: 조이스틱 문제에서 문자 변경과 커서 이동 계산을 메서드로 분리

--- src/Greedy/JoyStick.java
+++ src/Greedy/JoyStick.java
@@ -2,29 +2,42 @@ package Greedy;
 
 public class JoyStick {
     public int solution(String name) {
-        int moveCount = 0;
-        int minMove = name.length() - 1;
-
-        for (int i = 0; i < name.length(); i++) {
-
-            char c = name.charAt(i);
-            moveCount += Math.min(c - 'A', 'Z' - c + 1);
-
-            int next = i + 1;
-            while (next < name.length() && name.charAt(next) == 'A') {
-                next++;
-            }
-
-            minMove = Math.min(minMove, (i * 2) + (name.length() - next));
-            minMove = Math.min(minMove, (name.length() - next) * 2 + i);
+        int minSwitchCount = 0;
+        for (char c : name.toCharArray()) {
+            minSwitchCount += getMinSwitchCount(c);
         }
 
-        return moveCount + minMove;
+        return minSwitchCount + getMinMoveCount(name.length(), name);
+    }
+
+    private int getMinSwitchCount(char c) {
+        return Math.min(c - 'A', 'Z' - c + 1);
+    }
+
+    private int getMinMoveCount(int length, String name) {
+        int result = length - 1; //기본은 왼쪽에서 오른쪽으로 끝까지 이동
+
+        for (int i = 0 ; i < length; i++) {
+            int next = i + 1;
+
+            while (next < length && name.charAt(next) == 'A') {
+                next++;
+            }
+            // i : 오른쪽으로 간 거리 , next : 연속된 A 다음에 있는 다시 처리해야 할 위치
+            //오른쪽 -> 왼쪽
+            int move = i * 2 + (length - next);
+            //왼쪽 -> 오른쪽
+            int reverseMove = (length - next) * 2 + i;
+
+            result = Math.min(result, Math.min(move, reverseMove));
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
         JoyStick js = new JoyStick();
-        System.out.println(js.solution("JEROEN"));
-        System.out.println(js.solution("JAN"));
+        System.out.println(js.solution("JEROEN")); // 기대값: 56
+        System.out.println(js.solution("JAN"));    // 기대값: 23
     }
 }
